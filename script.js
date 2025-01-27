93e5983d9217d2c97248b4b1f290cc82d2560e21
TanmoyDhar1077 <tanmoydhar1077@gmail.com>
2025-01-28 01:03:37 +0600

    Hero and About Section Done

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menu = document.querySelector('.menu-links');
+    const hamburgerIcon = document.querySelector('.humburger-icon');
+    menu.classList.toggle('open');
+    hamburgerIcon.classList.toggle('open');
+}
