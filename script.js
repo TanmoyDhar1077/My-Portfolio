2a41748ceba21a0748ea17316f886567f2d27267
TanmoyDhar1077 <tanmoydhar1077@gmail.com>
2025-03-20 22:00:55 +0600

    Updated

--- script.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    const menu = document.querySelector('.menu-links');
-    const hamburgerIcon = document.querySelector('.humburger-icon');
-    menu.classList.toggle('open');
-    hamburgerIcon.classList.toggle('open');
-}
